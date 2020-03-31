8672ba691a764885217d67843a5ce3580e73fca8
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2020-03-31 03:42:56 +0200

    save yamnet features as npz, pred as h5

--- src/01_run-yamnet_covid19_20-03-30.py
+++ src/01_run-yamnet_covid19_20-03-30.py
@@ -1,6 +1,8 @@
 import sys
 
 import datetime
+import glob
+import h5py
 import numpy as np
 import os
 import tensorflow as tf
@@ -13,18 +15,21 @@ sys.path.append(yamnet_dir)
 import params  # noqa: E402
 import yamnet  # noqa: E402
 
-# Parse argument.
-# The only argument is the path to a directory containing SONYC recordings
-# for a given sensor
+# Parse arguments.
+# The first argument is the path to a directory containing SONYC recordings
+# for a given sensor.
+# The second argument is the path to the output directory for YAMNet features.
 args = sys.argv[1:]
 sensor_dir = str(args[0])
+out_dir = str(args[1])
 sensor_str = os.path.split(sensor_dir)[1]
+sonycnode_str = os.path.splitext(sensor_str)[0].split("-")[1]
 
 # Print header.
 start_time = int(time.time())
 print(str(datetime.datetime.now()) + " Start.")
-print("Running YAMNET on SONYC recordings for sensor " + sensor_str)
-print("Directory: " + sensor_str)
+print("Running YAMNET on SONYC recordings for sensor " + sonycnode_str)
+print("Directory: " + sensor_dir)
 print("")
 print("numpy version: {:s}".format(np.__version__))
 print("tensorflow version: {:s}".format(tf.__version__))
@@ -42,18 +47,29 @@ print("")
 # 1. first output is the convnet embedding (task-agnostic)
 # 2. second output is the audio event classification (task = AudioSet labels)
 tf.get_logger().setLevel('ERROR')
-yamnet_model = yamnet.yamnet_frames_model(params)
-yamnet_model_path = os.path.join(yamnet_dir, "yamnet.h5")
-yamnet_model.load_weights(yamnet_model_path)
-yamnet_multi_model = tf.keras.Model(
-    inputs=yamnet_model.inputs,
-    outputs=[yamnet_model.layers[-4].output, yamnet_model.output]
-)
+graph = tf.Graph()
+with graph.as_default():
+    yamnet_model = yamnet.yamnet_frames_model(params)
+    yamnet_model_path = os.path.join(yamnet_dir, "yamnet.h5")
+    yamnet_model.load_weights(yamnet_model_path)
+    yamnet_multi_model = tf.keras.Model(
+        inputs=yamnet_model.inputs,
+        outputs=[yamnet_model.layers[-4].output, yamnet_model.output]
+    )
 
 # Initialize HDF5 folder for prediction
-h5_path = os.path.join(sensor_dir, sensor_str + "_yamnet.h5")
+data_dir = os.path.split(sensor_dir)[0]
+out_pred_dir = os.path.join(data_dir, "covid_yamnet-pred")
+os.makedirs(out_pred_dir, exist_ok=True)
+h5_path = os.path.join(out_pred_dir, sonycnode_str + "_yamnet-pred.h5")
 
-# List SONYC recordings (NPY files)
+# Initialize NPZ folder for features
+out_features_dir = os.path.join(out_dir, "covid_yamnet-features")
+os.makedirs(out_features_dir, exist_ok=True)
+out_sensor_dir = os.path.join(out_features_dir, sensor_str)
+os.makedirs(out_sensor_dir, exist_ok=True)
+
+# List SONYC recordings (NPZ files)
 # These have been resampled to 16 kHz and converted to float32 by Mark Cartwright
 # The directory structure is <sensor_id>/<date>/<sensor_id>_<timestamp>_16k.npz
 # each npz file contains the signal x and the sample rate fs
@@ -66,11 +82,21 @@ for sonyc_path in sonyc_paths:
     sensor_timestamp_str = os.path.splitext(sonyc_name)[0]
     sensor_timestamp_split = sensor_timestamp_str.split("_")[:-1]
     sonyc_key = "_".join(sensor_timestamp_split)
-    yamnet_feature_name = sonyc_key + "_yamnet-features.npz"
-    yamnet_feature_path = os.path.join(date_dir, yamnet_feature_name)
     sonyc_npz = np.load(sonyc_path)
-    waveform = sonyc_npz["x"]
-    yamnet_feature, yamnet_pred = yamnet_multi_model.predict(waveform, steps=1)
+    waveform = sonyc_npz["x"][np.newaxis, :]
+    with graph.as_default():
+        yamnet_feature, yamnet_pred, _ = yamnet_multi_model.predict(
+            waveform, steps=1)
+    # Save feature.
+    date_str = os.path.split(date_dir)[1]
+    out_date_dir = os.path.join(out_sensor_dir, date_str)
+    os.makedirs(out_date_dir, exist_ok=True)
+    yamnet_feature_name = sensor_timestamp_str + "_yamnet-features.npz"
+    yamnet_feature_path = os.path.join(out_date_dir, yamnet_feature_name)
+    np.savez(yamnet_feature_path, yamnet_feature)
+    # Save prediction.
+    with h5py.File(h5_path, "a") as h5_file:
+        h5_file[sonyc_key] = yamnet_pred
 
 # Print elapsed time.
 print(str(datetime.datetime.now()) + " Finish.")
