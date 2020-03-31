e69749d51b27900ad27977d623aae3350bb67bbf
Vincent <vincent.lostanlen@nyu.edu>
2020-03-31 09:41:29 -0400

    run job 01 on HPC

---
+++ sbatch/01_run-yamnet_covid19_20-03-30/sbatch/01.sh
@@ -0,0 +1,24 @@
+# This shell script executes all Slurm jobsfor running YAMNet on SONYC recordings.
+
+sbatch 01_sonycnode-b827eb122f0f.sbatch
+sbatch 01_sonycnode-b827ebf31214.sbatch
+sbatch 01_sonycnode-b827eb3e842e.sbatch
+sbatch 01_sonycnode-b827ebc6dcc6.sbatch
+sbatch 01_sonycnode-b827eb42bd4a.sbatch
+sbatch 01_sonycnode-b827eb491436.sbatch
+sbatch 01_sonycnode-b827eb73e772.sbatch
+sbatch 01_sonycnode-b827eb815321.sbatch
+sbatch 01_sonycnode-b827ebefb215.sbatch
+sbatch 01_sonycnode-b827eb4e7821.sbatch
+sbatch 01_sonycnode-b827eb132382.sbatch
+sbatch 01_sonycnode-b827eb0fedda.sbatch
+sbatch 01_sonycnode-b827eb905497.sbatch
+sbatch 01_sonycnode-b827ebf9d204.sbatch
+sbatch 01_sonycnode-b827eb29eb77.sbatch
+sbatch 01_sonycnode-b827eb5895e9.sbatch
+sbatch 01_sonycnode-b827eb8e2420.sbatch
+sbatch 01_sonycnode-b827eb0d8af7.sbatch
+sbatch 01_sonycnode-b827eb4cc22e.sbatch
+sbatch 01_sonycnode-b827ebe3b72c.sbatch
+sbatch 01_sonycnode-b827eb1685c7.sbatch
+sbatch 01_sonycnode-b827eb5a9021.sbatch
